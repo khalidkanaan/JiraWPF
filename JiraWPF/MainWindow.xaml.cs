6f750f97d6b2ddcfe798aa7f23a9369675ec4500
Khalid Kanaan <khalid.kanaan.ca@gmail.com>
2024-02-14 13:06:46 -0500

    centered application upon starting up, made application window draggable

--- JiraWPF/MainWindow.xaml.cs
+++ JiraWPF/MainWindow.xaml.cs
@@ -34,5 +34,12 @@ namespace JiraWPF
         {
 
         }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
     }
 }
