ef1eb417201092119166f6b056d6029284c5c92c
Khalid Kanaan <khalid.kanaan.ca@gmail.com>
2024-02-22 17:08:10 -0500

    Added new Jira group filter ListBox with search functionality to facilitate filtering and selecting multiple options within the GetUsers view. Added RadioButtonTheme.xaml, CheckBoxTheme.xaml, and ListBoxTheme.xaml to enhance the styling of UI controls. Made username and email checkboxes selected by default in GetUsersViewModel.cs

--- JiraWPF/MVVM/View/GetUsersView.xaml.cs
+++ JiraWPF/MVVM/View/GetUsersView.xaml.cs
@@ -23,6 +23,44 @@ namespace JiraWPF.MVVM.View
         public GetUsersView()
         {
             InitializeComponent();
+
+            List<string> items = new List<string> { "None", "Checklist Item 2", "Checklist Item 3", "Checklist Item 4", "Checklist Item 5" };
+
+            // CollectionView for the ListBox
+            var collectionView = CollectionViewSource.GetDefaultView(items);
+            GroupListBox.ItemsSource = collectionView;
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            GroupFilterSearchTextBox.TextChanged += (s, _) =>
+            {
+                // store currently selected items
+                var selectedItems = new List<string>();
+                foreach (var item in GroupListBox.SelectedItems)
+                {
+                    selectedItems.Add((string)item);
+                }
+
+                // apply search filter
+                var collectionView = CollectionViewSource.GetDefaultView(GroupListBox.ItemsSource);
+                collectionView.Filter = item =>
+                {
+                    // when TextBox is empty, show all items
+                    if (string.IsNullOrEmpty(GroupFilterSearchTextBox.Text))
+                        return true;
+
+                    // Show only items that contain the text in the TextBox 
+                    return ((string)item).ToLower().Contains(GroupFilterSearchTextBox.Text.ToLower());
+                };
+
+                // reselect the previously selected items
+                foreach (var item in selectedItems)
+                {
+                    GroupListBox.SelectedItems.Add(item);
+                }
+            };
+        }
+
     }
 }
